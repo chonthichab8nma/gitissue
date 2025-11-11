88c2bdf34057bdc664195619eed223c6b5e9c6f7
chonthichab8nma <chonthichaboo2@gmail.com>
2025-11-11 10:40:41 +0700

    feat(todo): add mark as done feature

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-const todos = [];
-const input = document.getElementById('todo-input');
-const list = document.getElementById('todo-list');
-const btn = document.getElementById('add-btn');
-
-btn.addEventListener('click', () => {
-  if (input.value) {
-    todos.push(input.value);
-    renderTodos();
-    input.value = '';
-  }
-});
-
 function renderTodos() {
   list.innerHTML = '';
-  todos.forEach(todo => {
+  todos.forEach((todo, idx) => {
     const li = document.createElement('li');
     li.textContent = todo;
+    li.addEventListener('click', () => {
+      li.classList.toggle('done');
+    });
     list.appendChild(li);
   });
 }
